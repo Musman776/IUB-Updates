d14545e86940efa0adad5f11fae366ab5015e810
M.usman <usmanhnd4973@gmail.com>
2022-08-29 01:31:27 +0500

    IUB updates

---
+++ app/src/main/java/com/example/iubupdates/Home_screen.kt
@@ -0,0 +1,11 @@
+package com.example.iubupdates
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Home_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_screen)
+    }
+}
---
+++ app/src/main/java/com/example/iubupdates/Splash_Screen.kt
@@ -0,0 +1,19 @@
+package com.example.iubupdates
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+@Suppress("DEPRECATION")
+class Splash_Screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+        Handler().postDelayed({
+
+            startActivity(Intent(this@Splash_Screen,Home_screen::class.java))
+        },1000)
+
+    }
+}
