83926889e2a6e6f2881a36b0b8469ab22540e5e6
M.usman <usmanhnd4973@gmail.com>
2022-08-30 02:36:28 +0500

    IUB updates

--- app/src/main/java/com/example/iubupdates/PointActivity.kt
+++ app/src/main/java/com/example/iubupdates/PointActivity.kt
@@ -19,7 +19,7 @@ class PointActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_point)
-
+        supportActionBar?.hide()
 
 
 
