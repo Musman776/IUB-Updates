71f8665bb9fbfdf5b2d10765555672c2cb19b797
M.arslan <arslanapax@gmail.com>
2022-08-29 04:38:05 -0700

    option menu updated

--- app/src/main/java/com/example/iubupdates/Home_screen.kt
+++ app/src/main/java/com/example/iubupdates/Home_screen.kt
@@ -3,6 +3,8 @@ package com.example.iubupdates
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.home_screen.*
 
 class Home_screen : AppCompatActivity() {
@@ -14,5 +16,43 @@ class Home_screen : AppCompatActivity() {
 
             startActivity(Intent(this,Point_Schedule::class.java))
         }
+
+
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.opt_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+
+    }
+
+    //click on item
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.rate->{
+
+
+
+            }
+            R.id.more->{
+
+
+
+            }
+            R.id.notify->{
+
+
+
+            }
+            R.id.share->{
+
+
+
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
 }
